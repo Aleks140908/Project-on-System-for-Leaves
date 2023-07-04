666645ab76eb48e6128d9c4e9ca35945ae876923
Aleks140908 <135421872+Aleks140908@users.noreply.github.com>
2023-07-05 02:05:37 +0300

    Add files via upload
    DONE WITH THE FIRST OPTION .ONLY HAVE TO MAKE IT A TABLE.

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -13,6 +17,9 @@ public class Main {
     //2 state leave
     public static void showFirstOption(){
         try{
+
+
+            //Name!!!
             Scanner scanName = new Scanner(System.in);
             System.out.println("ENTER NAME:");
             String name = scanName.nextLine();
@@ -22,38 +29,70 @@ public class Main {
                     System.out.println("ERROR!There is a number in the name.");
                     showMenu();
                     scanNumber();
+                    Scanner scan = new Scanner(System.in);
+                    System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
+                    int num = scan.nextInt();
+                    checkOption(num);
                 }else{
                     break;
                 }
             }
 
+
+            //Email!!!
             Scanner scanEmail = new Scanner(System.in);
             System.out.println("ENTER EMAIL:");
             String email = scanEmail.nextLine();
 
+
+            //ID!!
             Scanner scanIdNum = new Scanner(System.in);
             System.out.println("ENTER IDENTIFICATION NUMBER: ");
             int idNumber = scanIdNum.nextInt();
 
-            Scanner scanBeginDate = new Scanner(System.in);
-            System.out.println("ENTER BEGINNING OF LEAVE: ");
-            double beginDate = scanBeginDate.nextInt();
 
-            Scanner scanEndDate = new Scanner(System.in);
-            System.out.println("ENTER END DATE OF LEAVE: ");
-            double endDate= scanEndDate.nextInt();
+            //Date Of Beginning!!!
+            Scanner scannerForDateOfBeginning = new Scanner(System.in);
+            System.out.println("Enter a date (yyyy-MM-dd): ");
+            String userInputForBeginningDate = scannerForDateOfBeginning.nextLine();
+            // To parse the users input
+            DateTimeFormatter formatterForBeginningDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
+
+            //Date Of End!!!
+            Scanner scannerForDateOfEnd = new Scanner(System.in);
+            System.out.println("Enter a date (yyyy-MM-dd): ");
+            String userInputForEndDate = scannerForDateOfBeginning.nextLine();
+            // To parse the users input
+            DateTimeFormatter formatterForEndDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+
+            //Knd of leave!!!
             Scanner scanPaidOrUnpaid = new Scanner(System.in);
             System.out.println("ENTER PAID OR UNPAID IS THE LEAVE: ");
             String paidOrUnpaid = scanPaidOrUnpaid.nextLine();
 
+            //Saving into a file!!!
+
+            try {
+                PrintStream ps = new PrintStream("OptionOnSaveInput.txt");
+                   ps.println(name);
+                   ps.print(email);
+                   ps.print(userInputForBeginningDate);
+                   ps.print(userInputForEndDate);
+                   ps.print(paidOrUnpaid);
+
+                ps.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            //FIGURE OUT HOW TO MAKE IT A TABLE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         }catch(Exception e){
             System.out.println("ERROR!Try again");
             showMenu();
             scanNumber();
+
         }
-
-
     }
     //3 look at leaves
     public static void showSecondOption(){
@@ -71,7 +110,25 @@ public class Main {
     public static void main(String[] args) {
         showMenu();
 
-        scanNumber();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
+        int num = scan.nextInt();
+
+        if (num == 1) {
+            showFirstOption();
+        } else if (num == 2) {
+            showSecondOption();
+        } else if (num == 3) {
+            showThirdOption();
+        } else if (num == 4) {
+            showFourthOption();
+        } else if (num == 5) {
+            showMenu();
+        } else {
+            System.out.println("!INVALID OPTION! PICK ANOTHER ONE");
+            showMenu();
+        }
+
 
     }
     //Scanner for what number the user picked
