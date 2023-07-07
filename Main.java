9ba5f398bf1a953da98dd80768a4892d31bba565
Aleks140908 <135421872+Aleks140908@users.noreply.github.com>
2023-07-08 01:10:21 +0300

    Add files via upload
    Fixed it up. Added scanner if the user wants to close the programme.

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 
 import java.io.*;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
@@ -14,13 +13,36 @@ public class Main {
         System.out.println("3. LOOK AT LEAVES FOR AN EMPLOYEE ");
         System.out.println("4. CHANGE STATUS FOR LEAVE ");
         System.out.println("5. EXIT ");
+        try{
+            Scanner scan = new Scanner(System.in);
+            System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
+            int num = scan.nextInt();
+
+            if (num == 1) {
+                showFirstOption();
+            } else if (num == 2) {
+                showSecondOption();
+            } else if (num == 3) {
+                showThirdOption();
+            } else if (num == 4) {
+                showFourthOption();
+            } else if (num == 5) {
+                showMenu();
+            } else {
+                System.out.println("!INVALID OPTION! PICK ANOTHER ONE");
+                showMenu();
+            }
+        }catch(Exception e){
+            System.out.println("ERROR!");
+            showMenu();
+        }
+
+
     }
-    //2 state leave - have to save all of them only
+    //2 state leave - have to save all of them only and fix the dates
     public static void showFirstOption(){
         try{
 
-
-            //Name - k
             Scanner scanName = new Scanner(System.in);
             System.out.println("ENTER NAME:");
             String name = scanName.nextLine();
@@ -29,29 +51,19 @@ public class Main {
                 if (Character.isDigit(c)) {
                     System.out.println("ERROR!There is a number in the name.");
                     showMenu();
-                    scanNumber();
-                    Scanner scan = new Scanner(System.in);
-                    System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
-                    int num = scan.nextInt();
-                    checkOption(num);
                 }else{
                     break;
                 }
             }
 
-
-            //Email - k
             Scanner scanEmail = new Scanner(System.in);
             System.out.println("ENTER EMAIL:");
             String email = scanEmail.nextLine();
 
-
-            //ID - k
             Scanner scanIdNum = new Scanner(System.in);
             System.out.println("ENTER IDENTIFICATION NUMBER: ");
             int IdNumber = scanIdNum.nextInt();
             String ID = String.valueOf(IdNumber);
-
 
             //Date Of Beginning - check if right
             Scanner scannerForDateOfBeginning = new Scanner(System.in);
@@ -61,10 +73,6 @@ public class Main {
                 Date dateBeg = dateFormatForBeg.parse(BeginningDate);
                 String dateStringBeg = dateFormatForBeg.format(dateBeg);
 
-
-
-
-
             //Date Of End - check if right
             Scanner scannerForDateOfEnd = new Scanner(System.in);
             System.out.println("Enter a date (yyyy-MM-dd): ");
@@ -73,8 +81,6 @@ public class Main {
             Date dateEnd = dateFormatForEnd.parse(BeginningDate);
             String dateStringEnd = dateFormatForEnd.format(dateEnd);
 
-
-            //Kind of leave - k
             Scanner scanPaidOrUnpaid = new Scanner(System.in);
             System.out.println("ENTER PAID OR UNPAID IS THE LEAVE: ");
             String paidOrUnpaid = scanPaidOrUnpaid.nextLine();
@@ -106,50 +112,34 @@ public class Main {
                     PrintStream ps = new PrintStream("OptionOnSaveInput.txt");
                     ps.println(TheFirstRowOfTheTable);
                     ps.println(TheSecondRowOfTheTable);
-
                     ps.close();
                 } catch (Exception exception) {
                     System.out.println("ERROR!");
                     exception.printStackTrace();
                     showMenu();
-                    Scanner scan = new Scanner(System.in);
-                    System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
-                    int num = scan.nextInt();
-
-                    switch (num){
-                        case 1: showFirstOption();
-                        case 2: showSecondOption();
-                        case 3: showThirdOption();
-                        case 4: showFourthOption();
-                        case 5: showMenu();
-                        default:
-                            System.out.println("ERROR!");
-                            showMenu();
-                    }
                 }
+
+                Scanner scanAnswer = new Scanner(System.in);
+            System.out.println("Would you like to continue using this programme(YES/NO): ");
+            String AnswerContinueProgramme = scanAnswer.nextLine();
+
+            if(AnswerContinueProgramme == "YES"){
+                System.out.println("You said yes");
+                showMenu();
+            }else if(AnswerContinueProgramme == "NO") {
+                System.out.println("Thank you for using this programme!");
+            }else{
+                System.out.println("The answer must be in caps!");
+                showFirstOption();
+            }
 
 
         }catch(Exception e){
             System.out.println("ERROR!Try again");
             showMenu();
-            Scanner scan = new Scanner(System.in);
-            System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
-            int num = scan.nextInt();
-
-            switch (num){
-                case 1: showFirstOption();
-                case 2: showSecondOption();
-                case 3: showThirdOption();
-                case 4: showFourthOption();
-                case 5: showMenu();
-                default:
-                    System.out.println("ERROR!");
-                    showMenu();
-            }
-
         }
     }
-               //3 look at leaves-ready
+    //3 look at leaves-ready
     public static void showSecondOption(){
 
         try (BufferedReader reader = new BufferedReader(new FileReader("OptionOnSaveInput.txt"))) {
@@ -161,23 +151,8 @@ public class Main {
             System.out.println("ERROR!");
             e.printStackTrace();
             showMenu();
-            scanNumber();
-            Scanner scan = new Scanner(System.in);
-            System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
-            int num = scan.nextInt();
-            switch (num){
-                case 1: showFirstOption();
-                case 2: showSecondOption();
-                case 3: showThirdOption();
-                case 4: showFourthOption();
-                case 5: showMenu();
-                default:
-                    System.out.println("ERROR!");
-                    showMenu();
-            }
         }
     }
-
 
     //4 look at employee leaves - not started
     public static void showThirdOption(){
@@ -197,62 +172,13 @@ public class Main {
         }catch (Exception e){
             System.out.println("ERROR!");
             showMenu();
-            Scanner scan = new Scanner(System.in);
-            System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
-            int num = scan.nextInt();
-            switch (num){
-                case 1: showFirstOption();
-                case 2: showSecondOption();
-                case 3: showThirdOption();
-                case 4: showFourthOption();
-                case 5: showMenu();
-                default:
-                    System.out.println("ERROR!");
-                    showMenu();
-            }
         }
     }
 
     public static void main(String[] args) {
         showMenu();
-
-        Scanner scan = new Scanner(System.in);
-        System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
-        int num = scan.nextInt();
-
-        switch (num){
-            case 1: showFirstOption();
-            case 2: showSecondOption();
-            case 3: showThirdOption();
-            case 4: showFourthOption();
-            case 5: showMenu();
-            default:
-                System.out.println("ERROR!");
-                showMenu();
-        }
-
-
     }
-    //Scanner for what number the user picked
-    public static void scanNumber(){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
-        int num = scan.nextInt();
-        checkOption(num);
-    }
-    //check what option is chosen
-    public static void checkOption(int num) {
-        switch (num){
-            case 1: showFirstOption();
-            case 2: showSecondOption();
-            case 3: showThirdOption();
-            case 4: showFourthOption();
-            case 5: showMenu();
-            default:
-                System.out.println("ERROR!");
-                showMenu();
-        }
-    }//below this is maybe worth it
+   //below this is maybe worth it
     public static void MakeNReadTable(String name,String email ,String ID,String BeginningDate,String EndDate,String paidOrUnpaid,String status){
         ArrayList<ArrayList<String>> tableForSavingInTheFirstOption = new ArrayList<>();
         ArrayList<String> TheFirstRowOfTheTable = new ArrayList<>();
@@ -281,26 +207,12 @@ public class Main {
             PrintStream ps = new PrintStream("OptionOnSaveInput.txt");
             ps.println(TheFirstRowOfTheTable);
             ps.println(TheSecondRowOfTheTable);
-
             ps.close();
         } catch (Exception exception) {
             System.out.println("ERROR!");
             exception.printStackTrace();
             showMenu();
-            Scanner scan = new Scanner(System.in);
-            System.out.print("ENTER NUMBER OF OPTION YOU WOULD LIKE: ");
-            int num = scan.nextInt();
 
-            switch (num){
-                case 1: showFirstOption();
-                case 2: showSecondOption();
-                case 3: showThirdOption();
-                case 4: showFourthOption();
-                case 5: showMenu();
-                default:
-                    System.out.println("ERROR!");
-                    showMenu();
-            }
         }
     }
 }
